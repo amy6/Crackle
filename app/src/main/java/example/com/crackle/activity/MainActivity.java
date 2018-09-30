374be6f6aef9b01115a195fc3d3a18534dc574ef
Mahima <mahima.6.ms@gmail.com>
2018-10-01 03:03:39 +0530

    add landscape layout for empty views

--- app/src/main/java/example/com/crackle/activity/MainActivity.java
+++ app/src/main/java/example/com/crackle/activity/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     @BindView(R.id.progressBar)
     ProgressBar progressBar;
     @BindView(R.id.errorLayout)
-    ConstraintLayout errorLayout;
+    ScrollView errorLayout;
     @BindView(R.id.errorImage)
     ImageView errorImage;
     @BindView(R.id.errorText)
