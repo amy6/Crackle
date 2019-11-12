06770a1984567fc2e80cd4befee3e06b385aae95
Mahima <mahima.6.ms@gmail.com>
2019-11-12 15:00:00 +0530

    migrate CreditResults data class to Kotlin

---
+++ app/src/main/java/example/com/crackle/model/CreditResults.kt
@@ -0,0 +1,8 @@
+package example.com.crackle.model
+
+import com.google.gson.annotations.SerializedName
+
+class CreditResults(@field:SerializedName("id")
+                    val id: Int, @field:SerializedName("cast")
+                    val castList: List<Cast>, @field:SerializedName("crew")
+                    val crewList: List<Crew>)
