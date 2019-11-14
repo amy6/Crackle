2300101af7cf3eea3ae2667626fed7d83604262a
Mahima <mahima.6.ms@gmail.com>
2019-11-14 16:21:13 +0530

    revert data class migration for Movie model class

--- app/src/main/java/example/com/crackle/model/Movie.kt
+++ app/src/main/java/example/com/crackle/model/Movie.kt
@@ -10,7 +10,7 @@ import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "movie")
-data class Movie : Parcelable {
+class Movie : Parcelable {
     @PrimaryKey
     @ColumnInfo(name = "movie_id")
     @SerializedName("id")
