d4a7c11430aace87a38a7f0c8503cd61a5501467
Mahima <mahima.6.ms@gmail.com>
2019-11-14 11:47:03 +0530

    optimize AppExecutors singleton class

--- app/src/main/java/example/com/crackle/utils/AppExecutors.kt
+++ app/src/main/java/example/com/crackle/utils/AppExecutors.kt
@@ -1,36 +1,20 @@
 package example.com.crackle.utils
 
-import android.os.Handler
-import android.os.Looper
-
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
-class AppExecutors(val diskIO: Executor, val networkIO: Executor, val mainThread: Executor) {
-
-    private class MainThreadExecutor : Executor {
-
-        private val mainThreadHandler = Handler(Looper.getMainLooper())
-
-        override fun execute(runnable: Runnable) {
-            mainThreadHandler.post(runnable)
-        }
-    }
+class AppExecutors private constructor(val diskIO: Executor) {
 
     companion object {
 
-        private val LOCK = Any()
+        @Volatile
         private var executorInstance: AppExecutors? = null
 
         fun getExecutorInstance(): AppExecutors {
-            if (executorInstance == null) {
-                synchronized(LOCK) {
-                    executorInstance = AppExecutors(Executors.newSingleThreadExecutor(),
-                            Executors.newFixedThreadPool(3),
-                            MainThreadExecutor())
-                }
+            return executorInstance ?: synchronized(this) {
+                AppExecutors(Executors.newSingleThreadExecutor())
+                        .also { executorInstance = it }
             }
-            return executorInstance!!
         }
     }
 }
